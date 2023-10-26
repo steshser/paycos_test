9368535ee76787b4ef2b24ffd4971478706e9acc
sksteshov <sksteshov@avito.ru>
2023-10-27 02:34:47 +0300

    update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,17 +6,14 @@ import pytest
 
 @pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
-    global driver
-    try:
-        browser = request.param
-        options = Options()
-        options.add_argument("--headless")
-        if browser == "chrome":
-            driver = webdriver.Chrome(options=options)
-        elif browser == "firefox":
-            driver = webdriver.Firefox(options=options)
-        else:
-            raise ValueError(f"Invalid browser specified: {browser}")
-        yield driver
-    finally:
-        driver.quit()
+    browser = request.param
+    options = Options()
+    options.add_argument("--headless")
+    if browser == "chrome":
+        driver = webdriver.Chrome(options=options)
+    elif browser == "firefox":
+        driver = webdriver.Firefox(options=options)
+    else:
+        raise ValueError(f"Invalid browser specified: {browser}")
+    yield driver
+    driver.quit()
