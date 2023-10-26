628630cf4ebd8b8ab1391dce7c841e7f16a1edea
sksteshov <sksteshov@avito.ru>
2023-10-27 00:34:15 +0300

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+import pytest
+
+
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    try:
+        browser = request.param
+        options = Options()
+        options.add_argument("--headless")
+        if browser == "chrome":
+            driver = webdriver.Chrome(options=options)
+        elif browser == "firefox":
+            driver = webdriver.Firefox(options=options)
+        else:
+            raise ValueError(f"Invalid browser specified: {browser}")
+        yield driver
+    finally:
+        driver.quit()
