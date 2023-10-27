0da8dd9e36041fc61d7a0b680f3d59a0e82e0854
sksteshov <sksteshov@avito.ru>
2023-10-27 11:59:21 +0300

    add chrome path to conftest.py

--- conftest.py
+++ conftest.py
@@ -8,7 +8,11 @@ import pytest
 def driver(request):
     browser = request.param
     if browser == "google-chrome":
-        driver = webdriver.Chrome()
+        chrome_path = "/usr/bin/google-chrome"
+        chrome_driver_path = "/usr/local/bin/chromedriver"
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = chrome_path
+        driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options)
     elif browser == "firefox":
         driver = webdriver.Firefox()
     else:
